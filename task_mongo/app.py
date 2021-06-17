dbe211a2274482500e9dcf2b697bfd280d54c745
Sakshi Jadhav <56793240+sakshijadhav92@users.noreply.github.com>
2021-06-17 23:33:39 +0530

    Update app.py

--- task_mongo/app.py
+++ task_mongo/app.py
@@ -17,11 +17,12 @@ def userform():
 @app.route("/adddata", methods=['POST'])  #GET --->POST
 def add_data():
     if request.method == 'POST':
+        userid = request.form.get("a")
         name = request.form.get("x")   #args--->form
         age = request.form.get("y")
         loc = request.form.get("z")
-        if name != "" and age != "" and loc != "":
-            user = user_collection.insert_one({"name": name, "age": age, "location": loc})
+        if userid != "" and name != "" and age != "" and loc != "":
+            user = user_collection.insert_one({"userid": userid,"name": name, "age": age, "location": loc})
             return ("data added to the database")
         else:
             return ("Kindly fill the form")
@@ -31,18 +32,18 @@ def read_data():
     user = (user_collection.find())
     return render_template('display.html', user=user)
 
-@app.route("/delete")
+@app.route("/delete", methods=['POST'])
 def delete_data():
     return render_template('delete.html')
 
 @app.route("/remove",methods=['POST'])
 def delete():
     if request.method == 'POST':
-        name = request.form.get("x")
-        if name != "":
-            user= user_collection.delete_one({"name": name})
+        userid = request.form.get("a")
+        if userid != "":
+            user= user_collection.delete_one({"userid": userid})
             return "Record deleted"
         else:
-            return "Kindly Enter Name"
+            return "Kindly Enter ID"
             
 app.run(port=5555,debug=True)
